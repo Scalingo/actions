29fdfb712eeae886c7513e06fa29ce1d881535f7
François KUBLER <francois+github@kubler.org>
2026-06-10 10:00:59 +0200

    chore(shellcheck): move script in its own file

---
+++ shellcheck/scripts/run-shellcheck.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+cd "${GITHUB_WORKSPACE}"
+
+if [ -n "${FILES}" ]; then
+	# Reset argv ($@):
+	set --
+
+	# Fill argv with the files provided. Keeps spaces.
+	while IFS= read -r file; do
+		[ -n "${file}" ] && set -- "$@" "${file}"
+	done <<-EOF
+	${FILES}
+	EOF
+
+	# Run ShellCheck
+	shellcheck -S "${SEVERITY}" -s bash "$@"
+else
+	# Default behavior
+	# Finds all .sh files and pass them to ShellCheck.
+	# Ignores those in the `.git` directory.
+	find . -type f -name '*.sh' -not -path './.git/*' \
+		-exec shellcheck -S "${SEVERITY}" -s bash {} +
+fi
